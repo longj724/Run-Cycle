7f0689900c9e411773e77fa90376cf47e822af10
juslong <juslong@umich.edu>
2020-01-03 14:55:33 -0500

    Deleting keys.js for git purposes

--- public/js/keys.js
+++
@@ -1,4 +0,0 @@
-var apiKey = { googleKey: 'AIzaSyBM15YPAs_MAsK_IHY5G1tEXZdSeyU7yJ0',
-    tomtomKey: 'cpFIgGMNPeo6cdumZnkGKYamnWPf6UA1',
-    mapBoxKey: 'pk.eyJ1IjoibG9uZ2oyNCIsImEiOiJjanlmM29uMnQwMHpuM25wYTRrNXVlNGg0In0.e22c1LT7Rl9FHcpLu7nGvQ'
-}
